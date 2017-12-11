32f2948bdfc2cbb83f46d465f725667c33ff0f87
yoat <yoatnic@me.com>
2017-12-11 21:38:41 +0900

    move to styles except animation

--- sample/src/App.js
+++ sample/src/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const appStyles = {
+  textAlign: 'center'
+};
+const logoStyles = {
+  animation: 'App-logo-spin infinite 20s linear',
+  height: '80px'
+};
+const headerStyles = {
+  backgroundColor: '#222',
+  height: '150px',
+  padding: '20px',
+  color: 'white'
+};
+const titleStyles = {
+  fontSize: '1.5em'
+};
+const introStyles = {
+  fontSize: 'large'
+};
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div style={appStyles}>
+        <header style={headerStyles}>
+          <img src={logo} alt="logo" style={logoStyles} />
+          <h1 style={titleStyles}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p style={introStyles}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
