fd5f4e0327b6234c8f79efdaaeb79368f8fedfee
yoat <yoatnic@me.com>
2017-12-12 21:31:36 +0900

    decompose component

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,38 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header'
+import Greet from './Greet'
 
 const appStyles = {
   textAlign: 'center'
 };
-const logoStyles = {
-  animation: 'App-logo-spin infinite 20s linear',
-  height: '80px'
-};
-const headerStyles = {
-  backgroundColor: '#222',
-  height: '150px',
-  padding: '20px',
-  color: 'white'
-};
-const titleStyles = {
-  fontSize: '1.5em'
-};
-const introStyles = {
-  fontSize: 'large'
-};
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div style={appStyles}>
-        <header style={headerStyles}>
-          <img src={logo} alt="logo" style={logoStyles} />
-          <h1 style={titleStyles}>Welcome to React</h1>
-        </header>
-        <p style={introStyles}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Greet message="Hello!"/>
       </div>
     );
   }
---
+++ sample/src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Header.css';
+
+const logoStyles = {
+  animation: 'App-logo-spin infinite 20s linear',
+  height: '80px'
+};
+const headerStyles = {
+  backgroundColor: '#222',
+  height: '150px',
+  padding: '20px',
+  color: 'white'
+};
+const titleStyles = {
+  fontSize: '1.5em'
+};
+
+const Header = function() {
+  return (
+    <header style={headerStyles}>
+      <img src={logo} alt="logo" style={logoStyles} />
+      <h1 style={titleStyles}>Welcome to React</h1>
+    </header>
+  );
+}
+
+export default Header;
