8154c2d4dee0e57a31a3c2fe5d821281f55602ed
yoat <yoatnic@me.com>
2017-12-25 20:49:07 +0900

    add wannas skelton

--- sample/src/molecules/Wannas.js
+++ sample/src/molecules/Wannas.js
@@ -1,8 +1,26 @@
 import React from "react";
 import Wanna from "../atom/Wanna";
 
-const Wannas = function() {
-  return <Wanna />;
-};
+class Wannas extends React.Component {
+  constructor() {
+    this.state = {
+      wannas: []
+    };
+  }
+  componentDidMount() {
+    return fetch("/wannas")
+      .then(responce => responce.json())
+      .then(responceJson => {
+        this.setState({
+          wannas: responceJson
+        });
+      });
+  }
+
+  render() {
+    const wannas = this.state.wannas.map(wanna => <Wanna {...wanna} />);
+    return <React.Fragment>{...wannas}</React.Fragment>;
+  }
+}
 
 export default Wannas;
