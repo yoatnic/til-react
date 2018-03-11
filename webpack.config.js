d12ba225da210ddadedb61ca347d3ca295fc8074
yoat <yoatnic@me.com>
2018-03-11 17:38:58 +0900

    update webopack v4

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,8 @@
 
 const path = require("path");
 const webpack = require("webpack");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
-const BASE_PLUGINS = [
-  new webpack.DefinePlugin({
-    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-  })
-];
 
 module.exports = {
-  entry:
-    process.env.NODE_ENV === "production"
-      ? ["babel-polyfill", "./src/index.js"]
-      : [
-          "babel-polyfill",
-          "react-hot-loader/patch",
-          "webpack-dev-server/client?http://localhost:8080",
-          "webpack/hot/only-dev-server",
-          "./src/index.js"
-        ],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
@@ -39,14 +22,6 @@ module.exports = {
       require("./serverapi/Wannatags")(app);
     }
   },
-  plugins:
-    process.env.NODE_ENV === "production"
-      ? BASE_PLUGINS.concat([new UglifyJsPlugin()])
-      : BASE_PLUGINS.concat([
-          new webpack.NamedModulesPlugin(),
-          new webpack.NoEmitOnErrorsPlugin(),
-          new webpack.HotModuleReplacementPlugin()
-        ]),
   module: {
     rules: [
       {
@@ -59,8 +34,5 @@ module.exports = {
         loaders: ["style-loader", "css-loader"]
       }
     ]
-  },
-  resolve: {
-    modules: [path.resolve(__dirname, "node_modules"), "node_modules"]
   }
 };
